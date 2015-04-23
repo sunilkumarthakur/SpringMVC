069994f6ea7fa47096ddef834f8008b84126e990
Hi <Hi@HP>
2015-04-23 15:49:58 +0530

    adding comment

--- Spring3MVC/src/main/java/com/mkyong/common/controller/JSONController.java
+++ Spring3MVC/src/main/java/com/mkyong/common/controller/JSONController.java
@@ -1,5 +1,5 @@
 package com.mkyong.common.controller;
-
+/* My controller */
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
